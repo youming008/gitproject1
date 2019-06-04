84fb8e0eb12d67bb3aceada4f1ed43080c79c3bd
youming <youming_lpg@163.com>
2019-06-04 10:48:02 +0800

    用git管理梦学谷项目

---
+++ spring-boot-01-hello/src/main/java/com/kingking/springboot/HelloMainApplication.java
@@ -0,0 +1,12 @@
+package com.kingking.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMainApplication {
+    public static void main(String [] args){
+        SpringApplication.run(HelloMainApplication.class,args);
+    }
+
+}

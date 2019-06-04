9e78c70389e7391661d11d7dd80c9961a50cd6df
youming <youming_lpg@163.com>
2019-06-04 17:26:41 +0800

    用git管理梦学谷项目3

---
+++ spring-boot-02-config/src/main/java/com/kingking/springboot/bean/Emp.java
@@ -0,0 +1,109 @@
+package com.kingking.springboot.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ConfigurationProperties 告诉springboot将配置文件中的对应属性值，映射到这个组件类中,进行一一绑定
+ *
+ * prefix = "emp" 配置文件中的前缀名，配置了那个前缀就会与下面的属性一一映射
+ *
+ * @Component  必须使用这个标签将当前组件作为 springboot中的一个组件来使用，这样才会纳入容器管理
+ *
+ */
+@Component
+@ConfigurationProperties(prefix="emp")
+public class Emp {
+    private String lastName;
+    private Integer age;
+    private Double salary;
+    private Boolean boss;
+    private Date birthday;
+
+    private Map map;
+    private List list;
+
+    private Forte forte;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Boolean getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Boolean boss) {
+        this.boss = boss;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public Forte getForte() {
+        return forte;
+    }
+
+    public void setForte(Forte forte) {
+        this.forte = forte;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", boss=" + boss +
+                ", birthday=" + birthday +
+                ", map=" + map +
+                ", list=" + list +
+                ", forte=" + forte +
+                '}';
+    }
+}
---
+++ spring-boot-02-config/src/test/java/com/kingking/springboot/SpringBoot02ConfigApplicationTests.java
@@ -0,0 +1,21 @@
+package com.kingking.springboot;
+
+import com.kingking.springboot.bean.Emp;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot02ConfigApplicationTests {
+    @Autowired
+    Emp emp;
+
+    @Test
+    public void contextLoads() {
+        System.out.println(emp);
+    }
+
+}

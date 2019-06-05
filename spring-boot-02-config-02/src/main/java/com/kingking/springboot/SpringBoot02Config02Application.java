879de0958a071d22bb89f09ae16592e75f919b39
youming <youming_lpg@163.com>
2019-06-05 18:00:27 +0800

    用git管理梦学谷配置学习4

--- spring-boot-02-config-02/src/main/java/com/kingking/springboot/SpringBoot02Config02Application.java
+++ spring-boot-02-config-02/src/main/java/com/kingking/springboot/SpringBoot02Config02Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBoot02Config02Application {
 
     public static void main(String[] args) {
+
+        System.out.println(System.getProperty("user.dir")); // 获取当先项目路径   也就是./ 是在哪个目录
+
         SpringApplication.run(SpringBoot02Config02Application.class, args);
     }
 

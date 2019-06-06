73f79d8496f95304e2ed3de3eb83060c4b81c33e
youming <youming_lpg@163.com>
2019-06-06 18:08:49 +0800

    用git管理梦学谷配置学习 logging

--- spring-boot-03-logging/src/test/java/com/kingking/springboot/SpringBoot03LoggingApplicationTests.java
+++ spring-boot-03-logging/src/test/java/com/kingking/springboot/SpringBoot03LoggingApplicationTests.java
@@ -2,6 +2,8 @@ package com.kingking.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBoot03LoggingApplicationTests {
 
+    //日志记录器
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void contextLoads() {
+        logger.trace("这是 trace 日志信息");
+
+        logger.debug("这是 debug 日志界别");
+
+
+        logger.info("这是  info  日志级别");
+
+        logger.warn("这是 warn 日志级别");
+
+        logger.error("这是  error 日志级别");
+
+
+
     }
 
 }
